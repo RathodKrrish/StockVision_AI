2e8a6cb3933c10a140a7ec3157f4001db5a99b15
Rathod Krish <krishrathod6089@gmail.com>
2026-05-12 18:50:58 +0530

    added environment variable

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -319,7 +319,7 @@ function Dashboard() {
         setForecastData([]);
 
         try {
-            const response = await fetch("http://127.0.0.1:8000/predict", {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/predict`, {
                 method: "POST",
 
                 headers: {
